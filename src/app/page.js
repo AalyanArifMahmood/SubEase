90d06cad80de6b1d7eededd4d1e8eca4b76a18cd
Timothylang1 <tlang@macalester.edu>
2024-02-24 12:12:55 -0600

    Template

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Template from "@/Listings/Template";
 
 export default function Home() {
   return (
+    <>
     <p>
       Hello world
     </p>
+    <Template/>
+    </>
   );
 }
