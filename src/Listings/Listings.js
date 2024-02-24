817fba7cfc1e91e7d601890e214b7298b0699597
AalyanArifMahmood <85511135+AalyanArifMahmood@users.noreply.github.com>
2024-02-24 11:52:09 -0600

    Firebase initialization

---
+++ src/Listings/Listings.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDRVw3h88OaQfxhP0sXorKT2wC8rZhoxDA",
+  authDomain: "subease-283bc.firebaseapp.com",
+  databaseURL: "https://subease-283bc-default-rtdb.firebaseio.com",
+  projectId: "subease-283bc",
+  storageBucket: "subease-283bc.appspot.com",
+  messagingSenderId: "845189068592",
+  appId: "1:845189068592:web:702bb21551610c2aa1faf5",
+  measurementId: "G-8J5SR6V4XJ"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
