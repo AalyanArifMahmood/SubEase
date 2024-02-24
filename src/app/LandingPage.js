e9d53e653010d6659e6e1ab9606386d7185bce97
gyaltsentenzin <40735388+gyaltsentenzin@users.noreply.github.com>
2024-02-24 17:49:56 -0600

    Landing page

---
+++ src/app/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+
+class LandingPage extends PureComponent {
+  render() {
+    return (
+      <div>LandingPage</div>
+    )
+  }
+}
+
+export default LandingPage;
+
