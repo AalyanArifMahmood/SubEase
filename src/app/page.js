7416c53282730aabcf9fd8a7eeb1abcab76e94cf
AalyanArifMahmood <85511135+AalyanArifMahmood@users.noreply.github.com>
2024-02-24 18:35:13 -0600

    page fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
 import styles from "./page.module.css";
 import Mainpage from "@/components/Mainpage/Mainpage";
 import LandingPage from "@/app/LandingPage";
-import AddListing from "@/app/AddListing/AddListing";
 
 export default function Home() {
   return (
     <>
-        {/*<Mainpage/>*/}
-        <AddListing/>
+        <Mainpage/>
         {/*<LandingPage/>*/}
     </>
   );
